5535ddb8a8135e48637a1e8bfce8bb178726e6c4
Erwin Caluag <erwin.caluag@digipay.ph>
2023-04-24 10:33:45 +0800

    Fixed title on loading screen

--- components/Loader.js
+++ components/Loader.js
@@ -8,9 +8,9 @@ const Loader = () => {
       <div className=" h-[100vh] bg-container absolute"></div>
       <Image src={Logo} alt="Lubak Tracker Logo" height={60}></Image>
       <h1 className="text-lg ">Welcome to </h1>
-      <h1 className="text-4xl uppercase font-semibold">Lubak Tracker</h1>
+      <h1 className="text-4xl uppercase font-semibold">Spotty Pots!</h1>
       <span class="loader mt-10"></span>
-      <h1 className="text-lg mt-4">Loading map...</h1>
+      <h1 className="text-lg mt-4">Loading map. Please wait...</h1>
     </div>
   );
 };
